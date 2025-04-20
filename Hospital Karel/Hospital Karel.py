141a010b46e9b2e56d33489af3acb96c9154f1be
aamna-ansari <aamnansari29@gmail.com>
2025-04-20 16:08:38 +0500

    Hospital Karel program

---
+++ Hospital Karel/Hospital Karel.py
@@ -0,0 +1,14 @@
+from karel.stanfordkarel import *
+
+# Here is a place to program your Section problem
+
+def main():
+    """
+    You should write your code to make Karel do its task in
+    this function. Make sure to delete the 'pass' line before
+    starting to write your own code. You should also delete this
+    comment and replace it with a better, more descriptive one.
+    """
+ 
+if __name__ == '__main__':
+    main()
