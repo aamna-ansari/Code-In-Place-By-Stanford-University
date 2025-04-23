910c7236a9484680e94ad8977f1605c92bc58f17
aamna-ansari <aamnansari29@gmail.com>
2025-04-23 20:14:22 +0500

    Piles

---
+++ Problems/Piles/Piles.py
@@ -0,0 +1,22 @@
+from karel.stanfordkarel import *
+
+# File: piles.py
+# -----------------------------
+# The warmup program defines a "main"
+# function which should make Karel
+# pick up all the beepers in the world.
+def main():
+    move()
+    # your code here
+    while front_is_clear():
+        for i in range(10): 
+            if beepers_present():
+                pick_beeper()
+            if no_beepers_present():
+                move()
+   
+   
+# don't edit these next two lines
+# they tell python to run your main function
+if __name__ == '__main__':
+    main()
